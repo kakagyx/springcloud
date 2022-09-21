0305632f74e8fa7eaa03e82300c96aa662f02780
kakagyx <kakagyx001@163.com>
2022-09-21 11:08:37 +0800

    集成Hystrix

--- eureka-parent/eureka-provider/src/main/java/cn/gyx/springcloud/eureka/provider/app/EurekaProviderApplication.java
+++ eureka-parent/eureka-provider/src/main/java/cn/gyx/springcloud/eureka/provider/app/EurekaProviderApplication.java
@@ -2,12 +2,14 @@ package cn.gyx.springcloud.eureka.provider.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan(basePackages = {"cn.gyx.springcloud.eureka.provider.*"})
+@EnableCircuitBreaker
 public class EurekaProviderApplication {
 
     public static void main(String[] args) {
--- eureka-parent/eureka-provider/src/main/java/cn/gyx/springcloud/eureka/provider/controller/ServiceProviderController.java
+++ eureka-parent/eureka-provider/src/main/java/cn/gyx/springcloud/eureka/provider/controller/ServiceProviderController.java
@@ -1,13 +1,33 @@
 package cn.gyx.springcloud.eureka.provider.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class ServiceProviderController {
 
+    @HystrixCommand(fallbackMethod = "TimeoutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    })
+
     @RequestMapping("/sysinfo")
     public String getSystemInfo(){
+
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         return "123123";
     }
+
+    public String TimeoutHandler() {
+        return "访问请求失败了，服务不可用";
+    }
+
 }
